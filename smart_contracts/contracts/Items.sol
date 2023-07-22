9ec1b46b3441f8dcaa299d3b8039c8ccb7fdf56c
vibonnet <82974617+vibonnet@users.noreply.github.com>
2023-07-22 21:54:29 +0200

    Update Items.sol

--- smart_contracts/contracts/Items.sol
+++ smart_contracts/contracts/Items.sol
@@ -1,1 +1,46 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.9;
 
+import "@openzeppelin/contracts@4.9.2/token/ERC721/ERC721.sol";
+import "@openzeppelin/contracts@4.9.2/token/ERC721/extensions/ERC721URIStorage.sol";
+import "@openzeppelin/contracts@4.9.2/access/Ownable.sol";
+
+contract Items is ERC721, ERC721URIStorage, Ownable {
+    constructor() ERC721("Items", "ITM") {}
+
+    function _baseURI() internal pure override returns (string memory) {
+        return "https://ipfs.io/ipfs/Qmdhw1bM5b81b1un9kvAArX5VQwePwy9KoysLHUuEk9p3d/";
+    }
+
+    function safeMint(address to, uint256 tokenId, string memory uri)
+        public
+        onlyOwner
+    {
+        _safeMint(to, tokenId);
+        _setTokenURI(tokenId, uri);
+    }
+
+    // The following functions are overrides required by Solidity.
+
+    function _burn(uint256 tokenId) internal override(ERC721, ERC721URIStorage) {
+        super._burn(tokenId);
+    }
+
+    function tokenURI(uint256 tokenId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (string memory)
+    {
+        return super.tokenURI(tokenId);
+    }
+
+    function supportsInterface(bytes4 interfaceId)
+        public
+        view
+        override(ERC721, ERC721URIStorage)
+        returns (bool)
+    {
+        return super.supportsInterface(interfaceId);
+    }
+}
